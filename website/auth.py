d110f1f2546baa7cc2f85861206026bfbf472d19
netanelye <netanelye@mta.ac.il>
2021-05-20 01:22:44 +0300

    navbar fix, sign_up page

--- website/auth.py
+++ website/auth.py
@@ -36,7 +36,7 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        first_name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         user = User.query.filter_by(email=email).first()
